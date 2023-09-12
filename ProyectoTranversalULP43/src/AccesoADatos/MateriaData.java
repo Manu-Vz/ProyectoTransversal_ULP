63f49530ca5703b9c1cd219270ac296a1044b539
Manuel Vasquez <manuvasquez2004@gmail.com>
2023-09-12 12:05:54 -0300

    Se Añadieron las bases para trabajar en MateriaData

--- ProyectoTranversalULP43/src/AccesoADatos/MateriaData.java
+++ ProyectoTranversalULP43/src/AccesoADatos/MateriaData.java
@@ -1,9 +1,18 @@
 package AccesoADatos;
 
+import org.mariadb.jdbc.Connection;
+
 /**
  *
  * @author manuv
  */
 public class MateriaData {
+    private Connection con = null;
+    
+    public MateriaData(){
+        con = Conectar.getConectar();
+    }
+    
+    //Metodos
     
 }
