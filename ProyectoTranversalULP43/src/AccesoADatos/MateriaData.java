5cc2ba0de9f44c1dac72cc009cd6c175775f0543
Manuel Vasquez <manuvasquez2004@gmail.com>
2023-09-12 12:21:41 -0300

    Primer Metodo de MateriaData

--- ProyectoTranversalULP43/src/AccesoADatos/MateriaData.java
+++ ProyectoTranversalULP43/src/AccesoADatos/MateriaData.java
@@ -1,6 +1,12 @@
 package AccesoADatos;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 import org.mariadb.jdbc.Connection;
+import proyectotranversalulp.Entidades.Materia;
 
 /**
  *
@@ -15,4 +21,31 @@ public class MateriaData {
     
     //Metodos
     
+    public void guardarMateria(Materia materia){
+        String sql = "insert into materia (nombre, anio, estado) "
+                + "values (?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getAnio());
+            ps.setBoolean(3, materia.isEstado());
+            
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()){
+                
+                materia.setIdMateria(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Materia Guardada Correctamente");
+                
+            }
+            ps.close();
+            
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al insertar datos " + ex.getMessage());
+        }
+        
+        
+    }
 }
