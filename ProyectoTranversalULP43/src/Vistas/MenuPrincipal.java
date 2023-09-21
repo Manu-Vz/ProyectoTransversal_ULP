d9cff8dcab4995205c727e0c5187dffd9b425fb3
Manuel Vasquez <manuvasquez2004@gmail.com>
2023-09-21 10:59:37 -0300

    Revert "Subo la última versión de el Menú principal"
    This reverts commit 12db54be48ca37120aaeb7678f6c53d2ab661a32.

--- ProyectoTranversalULP43/src/Vistas/MenuPrincipal.java
+++ ProyectoTranversalULP43/src/Vistas/MenuPrincipal.java
@@ -188,7 +188,4 @@ public class MenuPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuItem jmiGestionAlumnos;
     private javax.swing.JMenuItem jmiManejoInscripciones;
     // End of variables declaration//GEN-END:variables
-
-    
-
 }
